b61819634d5a4429a8136c4667ded27d0d0529f1
kckhafra <kckhafra@gmail.com>
2021-09-13 08:40:05 -0400

    combined the two

--- hash_collision.py
+++ hash_collision.py
@@ -12,50 +12,40 @@ def hash_collision(k):
     #Collision finding code goes here
     x = os.urandom(64)
     y = os.urandom(64)
-
-    scale=16
-    num_of_bits = 8
     xHash = hashlib.sha256()
     yHash = hashlib.sha256()
-    xBitsTotal=""
-    yBitsTotal=""
-  
+    scale = 16 ## equals to hexadecimal
+    num_of_bits = 4
     for i in x:
         xHash.update(str(bin(i)[2:].zfill(num_of_bits)).encode('utf-8'))
 
     for m in y:
         yHash.update(str(bin(m)[2:].zfill(num_of_bits)).encode('utf-8'))
-        
-    for xNum in xHash.hexdigest():
-        xBitsTotal=xBitsTotal+bin(int(xNum,scale))[2:].zfill(4)
-        
-    for yNum in yHash.hexdigest():
-        yBitsTotal=yBitsTotal+bin(int(yNum,scale))[2:].zfill(4)
-        
+    xBitsTotal=bin(int(xHash.hexdigest(), scale))[2:]
+    yBitsTotal=bin(int(yHash.hexdigest(), scale))[2:]
+    
+    # print("X: ",xBitsTotal)
+    # print("Y: ",yBitsTotal)
+    
     match=False
     while(match==False):
         newK=len(xBitsTotal)-k
-        yLastKbits=yBitsTotal[newK:len(yBitsTotal)]
-        xLastKbits=xBitsTotal[newK:len(xBitsTotal)]
-        # print("yLastKbits",yLastKbits)
-        # print("xLastKbits",xLastKbits)
+        yLastKbits=yBitsTotal[newK:len(yBitsTotal)+1]
+        xLastKbits=xBitsTotal[newK:len(xBitsTotal)+1]
+        # print("YLAST: ",yLastKbits)
+        # print("XLAST: ",xLastKbits)
         if(yLastKbits==xLastKbits):
             match=True
         else:
             y = os.urandom(64)
-            yBitsTotal=""
-            
-            for o in y:
-                yHash = hashlib.sha256("0".encode('utf-8'))
-                yHash.update(str(bin(o)[2:].zfill(num_of_bits)).encode('utf-8'))
-            
-            for yNumRep in yHash.hexdigest():
-                yBitsTotal=yBitsTotal+bin(int(yNumRep,scale))[2:].zfill(4)
-            
+            yBitsTotal=bin(int(hashlib.sha256(y).hexdigest(), scale))[2:].zfill(num_of_bits)
+        
     x=xBitsTotal.encode('utf-8')
     y=yBitsTotal.encode('utf-8')
+    # print("X: ",x)
+    # print("Y: ",y)
     return( x, y )
 
-# hash_collision(5)
 
+hash_collision(15)
 
