079ffc7c102e4199035431078cf4b6e314471cdc
kckhafra <kckhafra@gmail.com>
2021-09-13 13:34:13 -0400

    added hash_preimage

--- hash_collision.py
+++ hash_collision.py
@@ -14,34 +14,8 @@ def hash_collision(k):
     y = b'\x00'
     x = os.urandom(64)
     y = os.urandom(64)
-    # xHash = hashlib.sha256()
-    # yHash = hashlib.sha256()
     scale = 16 ## equals to hexadecimal
-    num_of_bits = 8
-    xBitsTotal=""
-    yBitsTotal=""
-    xHash=hashlib.sha256(x).hexdigest()
-    yHash=hashlib.sha256(y).hexdigest()
-    # for i in x:
-    #     xHash.update(str(bin(i)[2:].zfill(num_of_bits)).encode('utf-8'))
 
-    # for m in y:
-    #     yHash.update(str(bin(m)[2:].zfill(num_of_bits)).encode('utf-8'))
-    
-    # for xNum in xHash:
-    #     xBitsTotal=xBitsTotal+bin(int(xNum,scale))[2:].zfill(4)
-    #     print(xBitsTotal)
-    #     # print(xNum)
-        
-    # for yNum in yHash:
-    #     yBitsTotal=yBitsTotal+bin(int(yNum,scale))[2:].zfill(4)
-        # print(yBitsTotal)
-
-    # xBitsTotal=bin(int(xHash.hexdigest(), scale))[2:]
-    # yBitsTotal=bin(int(yHash.hexdigest(), scale))[2:]
-    
-    # print("X: ",xBitsTotal)
-    # print("Y: ",yBitsTotal)
     xBitsTotal=bin(int(hashlib.sha256(x).hexdigest(), scale))[2:]
     yBitsTotal=bin(int(hashlib.sha256(y).hexdigest(), scale))[2:]
     
@@ -50,22 +24,15 @@ def hash_collision(k):
         newK=len(xBitsTotal)-k
         yLastKbits=yBitsTotal[newK:len(yBitsTotal)]
         xLastKbits=xBitsTotal[newK:len(xBitsTotal)]
-        # print("YLAST: ",yLastKbits)
-        # print("XLAST: ",xLastKbits)
+
         if(yLastKbits==xLastKbits):
             match=True
         else:
             y = os.urandom(64)
             yBitsTotal=bin(int(hashlib.sha256(y).hexdigest(), scale))[2:]
-            
-            
-            
-        
-    # x=xBitsTotal.encode('utf-8')
-    # y=yBitsTotal.encode('utf-8')
-    # print("X: ",x)
-    # print("Y: ",y)
+    print(xBitsTotal)
+    print(yBitsTotal)
     return( x, y )
 
 
-# hash_collision(5)
+hash_collision(20)
